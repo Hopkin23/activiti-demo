847e349f70f996345a2f5f73f25a8d0e1043d369
liuhp <hp.l@qq.com>
2023-02-12 13:00:37 +0800

    引入Activiti

--- src/main/java/com/hopkin/activitidemo/ActivitiDemoApplication.java
+++ src/main/java/com/hopkin/activitidemo/ActivitiDemoApplication.java
@@ -3,7 +3,10 @@ package com.hopkin.activitidemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+        org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
+        org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class
+})
 public class ActivitiDemoApplication {
 
     public static void main(String[] args) {
